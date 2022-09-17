2690127e8c24954cbc8b465ec24909029cfd7924
Matthew Gladkov <matthew.gladkov@gmail.com>
2022-09-17 20:41:34 +0300

    fixed bug

--- run_script.ps1
+++ run_script.ps1
@@ -1,4 +1,4 @@
 ﻿$Trigger= New-ScheduledTaskTrigger -At 8:00pm -Daily
 $User= "NT AUTHORITY\SYSTEM"
-$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "python $PSScriptRoot main.py"
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "python $PSScriptRoot\main.py"
 Register-ScheduledTask -TaskName "StartupScript_PS" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force
